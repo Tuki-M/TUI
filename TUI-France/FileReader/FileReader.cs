43678eceba136215bf299f2895019d07233d3384
Tuki-M <men-inttic@hotmail.fr>
2018-05-02 23:26:34 +0200

    [Exercise - 3] A user should be able to read an XML file

--- TUI-France/FileReader/FileReader.cs
+++ TUI-France/FileReader/FileReader.cs
@@ -8,7 +8,22 @@ namespace FileReader
 {
     public class FileReader : FileReaderAbstract
     {
+        /// <summary>
+        /// Read text file from the path
+        /// </summary>
+        /// <param name="path">full path to text file to read</param>
+        /// <returns>the content of the file to read</returns>
         public string ReadTextFile(string path)
+        {
+            return ReadFile(path);
+        }
+
+        /// <summary>
+        /// Read XML file from the path
+        /// </summary>
+        /// <param name="path">full path to XML file to read</param>
+        /// <returns>the content of the file to read</returns>
+        public string ReadXmlFile(string path)
         {
             return ReadFile(path);
         }
