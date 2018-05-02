84918d17727a7a2226f31a858a2b297a8ac35716
Tuki-M <men-inttic@hotmail.fr>
2018-05-03 00:00:31 +0200

    [Exercise - 3] A user should be able to read XML file in role based security context

--- TUI-France/FileReader/FileReader.cs
+++ TUI-France/FileReader/FileReader.cs
@@ -26,10 +26,19 @@ namespace FileReader
         /// Read XML file from the path
         /// </summary>
         /// <param name="path">full path to XML file to read</param>
+        /// <param name="role">role based security context, by default it's None</param>
         /// <returns>the content of the file to read</returns>
-        public string ReadXmlFile(string path)
+        public string ReadXmlFile(string path, Role role = Role.None)
         {
-            return ReadFile(path);
+            switch (role)
+            {
+                case Role.None:
+                    return ReadFile(path);
+                case Role.Admin:
+                    return ReadFile(path);//Not specified clearly by the business for now
+                default:
+                    return ReadFile(path);
+            }
         }
     }
 }
