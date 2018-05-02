3482d7bda75cdc8b18b4f02c7d0fcee5f33d1a5e
Tuki-M <men-inttic@hotmail.fr>
2018-05-03 00:14:34 +0200

    [Exercise - 3] Enable role based security context for Text files

--- TUI-France/FileReader/FileReader.cs
+++ TUI-France/FileReader/FileReader.cs
@@ -13,13 +13,21 @@ namespace FileReader
         /// </summary>
         /// <param name="path">full path to text file to read</param>
         /// <param name="isEncrypted">if file is encrypted, by default it's not</param>
+        /// <param name="role">role based security context, by default it's None</param>
         /// <returns>the content of the file to read</returns>
-        public string ReadTextFile(string path, bool isEncrypted = false)
+        public string ReadTextFile(string path, bool isEncrypted = false, Role role = Role.None)
         {
-            if (!isEncrypted)
-                return ReadFile(path);
-            else
-                return ReadEncryptedFile(path);
+            switch (role)
+            {
+                case Role.None:
+                    return !isEncrypted ? ReadFile(path) : ReadEncryptedFile(path);
+
+                case Role.Admin: //Not specified clearly by the business for now
+                    return !isEncrypted ? ReadFile(path) : ReadEncryptedFile(path);
+
+                default:
+                    return !isEncrypted ? ReadFile(path) : ReadEncryptedFile(path);
+            }
         }
 
         /// <summary>
