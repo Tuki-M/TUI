190ef6ed244029e34f4fdc6ea089f744475be61a
Tuki-M <men-inttic@hotmail.fr>
2018-05-02 23:19:33 +0200

    [Exercise - 3] A user should be able to read a text file

---
+++ TUI-France/FileReader/FileReader.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FileReader
+{
+    public class FileReader : FileReaderAbstract
+    {
+        public string ReadTextFile(string path)
+        {
+            return ReadFile(path);
+        }
+    }
+}
---
+++ TUI-France/FileReader/FileReaderAbstract.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FileReader
+{
+    public abstract class FileReaderAbstract
+    {
+        public string ReadFile(string path)
+        {
+            if (!File.Exists(path))
+                throw new FileNotFoundException("File not found");
+
+            return File.ReadAllText(path);
+        }
+    }
+}
